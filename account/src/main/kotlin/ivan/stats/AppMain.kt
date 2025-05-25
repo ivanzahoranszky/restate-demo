e592f981dc230dabd9f5a5386b0f6736d0b1e967
ivan.zahoranszky <ivan.zahoranszky@gmail.com>
2025-05-25 17:19:04 +0400

    Adding payment and account services

--- account/src/main/kotlin/ivan/stats/AppMain.kt
+++ account/src/main/kotlin/ivan/stats/AppMain.kt
@@ -3,7 +3,6 @@ package ivan.stats
 import dev.restate.sdk.endpoint.Endpoint
 import dev.restate.sdk.http.vertx.RestateHttpServer
 import ivan.stats.service.AccountService
-import ivan.stats.service.StatsService
 import org.koin.core.context.startKoin
 import org.koin.java.KoinJavaComponent.getKoin
 
